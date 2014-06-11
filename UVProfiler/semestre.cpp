adb496324f7ce90c4948bd8d586dad944af2f999
alexandre <alexandre.mirgalet@gmail.com>
2014-06-11 21:39:58 +0200

    Petites modifs de semestre...

--- UVProfiler/semestre.cpp
+++ UVProfiler/semestre.cpp
@@ -3,26 +3,24 @@
 void Semestre::initSemestreSystem() {
     qRegisterMetaTypeStreamOperators<Semestre>("Semestre");
     qMetaTypeId<Semestre>(); // Teste la validité de la classe EnsCredits
+    qRegisterMetaTypeStreamOperators<Semestre::Saison>("Saison");
+    qMetaTypeId<Semestre::Saison>(); // Teste la validité de la classe EnsCredits
 }
 
 QDataStream& operator<< (QDataStream& out, const Semestre& Valeur) {
-    out //<< Valeur._saison
+    out << Valeur._saison
         << Valeur._annee;
     return out;
 }
 
 QDataStream& operator>> (QDataStream& in, Semestre& Valeur){
-    // in >> Valeur._saison; // <- en cours d'implémentation, mais plante encore
+    in >> Valeur._saison; // <- en cours d'implémentation, mais plante encore
     in >> Valeur._annee;
     return in;
 }
-/*
+
 QDataStream& operator<<(QDataStream& out, Semestre::Saison s) {
-    switch (s) {
-    case A: return out << "A";
-    case P: return out << "P";
-    default: return out << (int) s;
-    }
+    out<<s;
     return out;
 }
 
--- UVProfiler/semestre.h
+++ UVProfiler/semestre.h
@@ -8,16 +8,15 @@
 class Semestre
 {
 protected:
-    enum Saison {A, P};
-    //Q_DECLARE_METATYPE(Saison)
-    //QDataStream& operator<<(QDataStream& out, Semestre::Saison s);
-    //QDataStream& operator>> (QDataStream& in, Semestre::Saison& s);
+
+
 
     Saison _saison;
     unsigned int _annee;
 public:
     Semestre(Saison saison = A, unsigned int annee=00) : _saison(saison), _annee(annee) {}
     Semestre(const Semestre& copie) : _saison(copie._saison), _annee(copie._annee) {}
+    enum Saison {A, P};
 
     static void initSemestreSystem();
     friend QDataStream& operator<< (QDataStream&, const Semestre&);
@@ -27,6 +26,10 @@ public:
 Q_DECLARE_METATYPE(Semestre)
 QDataStream& operator<< (QDataStream& out, const Semestre& Valeur);
 QDataStream& operator>> (QDataStream& in, Semestre& Valeur);
+
+Q_DECLARE_METATYPE(Semestre::Saison)
+QDataStream& operator<<(QDataStream& out, Semestre::Saison s);
+QDataStream& operator>> (QDataStream& in, Semestre::Saison& s);
 
 
 #endif // SEMESTRE_H
